3f2d535508ac41116f7d75ff65df3cb2591e84c9
axel <natandreik11@gmail.com>
2025-05-12 15:07:36 -0400

    nuevo texto

---
+++ for-normal.py
@@ -0,0 +1,2 @@
+for pepito in range(10):
+    print("no devo botar basura al suelo", pepito)
---
+++ progra_propio.py
@@ -0,0 +1,9 @@
+
+edad = int(print("pon tu edad"))
+año_actual = 2025
+año_nacimiento = edad - año_actual
+print("este es tu año de nacimiento", año_nacimiento)
+
+#except ValueError:
+#print("tiene que ser un numero entero")
+
